c33e3eb4d7d47cdca67470f4743d3502470b7905
martins86 <martins86@gmail.com>
2021-09-29 16:18:00 +0000

    imports

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -8,6 +8,23 @@ import ReactDOM from "react-dom";
 //   document.getElementById('root')
 // );
 
-import BomDia from "./componentes/bomDia";
+// import BomDia from "./componentes/bomDia";
 
-ReactDOM.render(<BomDia nome="Thiago" />, document.getElementById("root"));
+// ReactDOM.render(<BomDia nome="Thiago" />, document.getElementById("root"));
+
+// import Mult from "./componentes/multiplos";
+
+// ReactDOM.render(
+//   <div>
+//     <Mult.BoaTarde nome="Thiago" />
+//     <Mult.BoaNoite nome="Martins" />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+import Saudacao from './componentes/saudacao';
+
+ReactDOM.render(
+    <Saudacao tipo="Bom Dia" nome="Ana" />, 
+    document.getElementById("root")
+);
