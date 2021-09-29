da9da66dded0560e3c7cfe043ddc5639436370ca
martins86 <martins86@gmail.com>
2021-09-29 16:16:42 +0000

    componente multiplos em um

---
+++ exercicios/src/componentes/multiplos.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BoaTarde = (props) => <h1>Boa Tarde {props.nome}!</h1>;
+
+const BoaNoite = (props) => <h1>Boa Noite {props.nome}!</h1>;
+
+const Mult = { BoaTarde, BoaNoite };
+
+export default Mult;
