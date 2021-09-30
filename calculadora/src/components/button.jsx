686257218b30efee4520575ae9610fc754b812e2
martins86 <martins86@gmail.com>
2021-09-30 21:52:18 +0000

    aplicando estilos e classes aos botões e iniciando funcionalidades

--- calculadora/src/components/button.jsx
+++ calculadora/src/components/button.jsx
@@ -2,6 +2,20 @@ import React from "react";
 
 import "./button.css";
 
-const Button = (props) => <button className="button">{props.label}</button>;
+const Button = (props) => {
+  let stylesClass = "button ";
+  stylesClass += props.operation ? "operation" : "";
+  stylesClass += props.double ? "double" : "";
+  stylesClass += props.triple ? "triple" : "";
+
+  return (
+    <button
+      onClick={() => props.click(props.label)}
+      className={stylesClass}
+    >
+      {props.label}
+    </button>
+  );
+};
 
 export default Button;
