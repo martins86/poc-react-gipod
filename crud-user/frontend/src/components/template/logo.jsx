bff74f5f18c4a6c507042499d07ae712242db685
martins86 <martins86@gmail.com>
2021-10-04 20:28:27 +0000

    ajustando css componente logo

--- crud-user/frontend/src/components/template/logo.jsx
+++ crud-user/frontend/src/components/template/logo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 import "./logo.css";
+import logo from "./../../assets/images/logo.svg";
 
-const Logo = (props) => <aside className="logo">Logo</aside>;
+const Logo = (props) => (
+  <aside className="logo">
+    <a href="/">
+      <img src={logo} alt="Logo React" />
+    </a>
+  </aside>
+);
 
 export default Logo;
