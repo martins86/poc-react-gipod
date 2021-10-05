3d5942a4aa8c3d542bac90cf2d550053facc4840
martins86 <martins86@gmail.com>
2021-10-05 16:53:27 +0000

    finalizando o footer

--- crud-user/frontend/src/components/template/footer.jsx
+++ crud-user/frontend/src/components/template/footer.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import "./footer.css";
 
-const Footer = (props) => <footer className="footer">Rodapé</footer>;
+const Footer = (props) => (
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong> TMF</strong>
+    </span>
+  </footer>
+);
 
 export default Footer;
